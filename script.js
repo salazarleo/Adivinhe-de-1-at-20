83ae66bb9ab56c8f1478bc4d5636e0d40079dc53
salazarleo <leonardomeg123@gmail.com>
2024-03-07 13:36:50 -0300

    alteração final

--- script.js
+++ script.js
@@ -10,39 +10,38 @@ console.log(document.querySelector('.guess').value = 89)
 
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1
-
-
+let secretNumber = Math.trunc(Math.random() * 20) + 1
 let score = 20
+let highscore = 0
+
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message
+}
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
+
+
     if (!guess) {
-        document.querySelector('.message').textContent = 'Sem numero!'
+        displayMessage('Sem numero!')
 
     } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Parabens, você acertou!'
-
+        displayMessage('Parabens, você acertou!')
         document.querySelector('body').style.backgroundColor = '#60b347'
-
         document.querySelector('.number').style.width = '30rem'
         document.querySelector('.number').textContent = secretNumber
 
-    } else if (guess > secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Muito Alto!'
-            score--
-            document.querySelector('.score').textContent = score
-        } else {
-            document.querySelector('.message').textContent = 'Você perdeu!'
-            document.querySelector('body').style.backgroundColor = 'red'
-            document.querySelector('.number').style.width = '30rem'
+        if (score > highscore) {
+            highscore = score
+            document.querySelector('.highscore').textContent = highscore
         }
+    }
 
-    } else if (guess < secretNumber) {
+
+    else if (guess !== secretNumber) {
         if (score > 1) {
-            document.querySelector('.message').textContent = 'Muito Baixo!'
+            displayMessage(guess > secretNumber ? 'Muito Alto!' : 'Muito Baixo!')
             score--
             document.querySelector('.score').textContent = score
         } else {
@@ -53,4 +52,13 @@ document.querySelector('.check').addEventListener('click', function () {
     }
 })
 
-
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1
+    document.querySelector('.score').textContent = score
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.guess').value = ''
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.number').style.width = '15rem'
+    document.querySelector('.message').textContent = 'Comece a adivinhar...'
+})
